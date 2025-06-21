ce797b497fa7801b8e703c71c7d1d47c82677544
mariam-alaa20031 <109764181+mariam-alaa20031@users.noreply.github.com>
2025-06-22 00:52:25 +0300

    modified cartDrawer component name in defineComponent and productCard

--- saqaya-ecommerce/src/components/productCard.vue
+++ saqaya-ecommerce/src/components/productCard.vue
@@ -17,6 +17,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import productPrice from "./productPrice.vue";
+
 export default defineComponent({
   name: "productCard",
   components: {
