55b04ed7d4825407f0953133bbe8f15553a52a5c
mariam-alaa20031 <109764181+mariam-alaa20031@users.noreply.github.com>
2025-06-29 20:45:00 +0300

    add tests for remaining ratings

--- saqaya-ecommerce/tests/unit/products/productRating.spec.ts
+++ saqaya-ecommerce/tests/unit/products/productRating.spec.ts
@@ -17,6 +17,39 @@ describe('Dropdown sorting UI component', () => {
 
   });
 
+  it('attaches correct classes for a rating of 2', async () => {
+       await wrapper.setProps({rating:1})
+       const stars= wrapper.findAll("svg")
+       expect(stars[0].classes()).toEqual(['star'])
+       expect(stars[1].classes()).toEqual(['star'])
+       expect(stars[2].classes()).toEqual(['blank'])
+       expect(stars[3].classes()).toEqual(['blank'])
+       expect(stars[4].classes()).toEqual(['blank'])
+
+  });
+
+    it('attaches correct classes for a rating of 3', async () => {
+       await wrapper.setProps({rating:1})
+       const stars= wrapper.findAll("svg")
+       expect(stars[0].classes()).toEqual(['star'])
+       expect(stars[1].classes()).toEqual(['star'])
+       expect(stars[2].classes()).toEqual(['star'])
+       expect(stars[3].classes()).toEqual(['blank'])
+       expect(stars[4].classes()).toEqual(['blank'])
+
+  });
+
+    it('attaches correct classes for a rating of 4', async () => {
+       await wrapper.setProps({rating:1})
+       const stars= wrapper.findAll("svg")
+       expect(stars[0].classes()).toEqual(['star'])
+       expect(stars[1].classes()).toEqual(['star'])
+       expect(stars[2].classes()).toEqual(['star'])
+       expect(stars[3].classes()).toEqual(['star'])
+       expect(stars[4].classes()).toEqual(['blank'])
+
+  });
+
   it('attaches correct classes for a rating of 5', async () => {
        await wrapper.setProps({rating:5})
        const stars= wrapper.findAll("svg")
