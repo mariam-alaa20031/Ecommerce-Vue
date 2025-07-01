e5eda1014cd3922270b1aff9a236518296a6c6c6
mariam-alaa20031 <109764181+mariam-alaa20031@users.noreply.github.com>
2025-07-01 19:06:34 +0300

    migrate productsPage to composition

--- saqaya-ecommerce/src/components/cart/cartDrawer.vue
+++ saqaya-ecommerce/src/components/cart/cartDrawer.vue
@@ -1,52 +1,49 @@
 <template>
   <div>
-    <div
-      v-if="visible"
-      class="overlay"
-      @click="closeDrawer"
-    ></div>
+    <div v-if="visible" class="overlay" @click="closeDrawer"></div>
 
     <div class="drawer" :class="{ open: visible }" @click.stop>
       <h2>Cart</h2>
       <div class="drawer__line"></div>
-      <p
-      v-if="cart.length==0">Your cart is empty, take a look at our products.</p>
-      <cartItem v-else ></cartItem>
+      <p v-if="cart.length == 0">
+        Your cart is empty, take a look at our products.
+      </p>
+      <cartItem v-else></cartItem>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import {useStore} from 'vuex'
-import {Product} from '../../../public/interfaces/Product';
-import cartItem from './cartItem.vue'
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+import { Product } from "../../../public/interfaces/Product";
+import cartItem from "./cartItem.vue";
 
 export default defineComponent({
-  name: 'cartDrawer',
-  components:{
-     cartItem
+  name: "cartDrawer",
+  components: {
+    cartItem,
   },
   props: {
-    visible: Boolean
+    visible: Boolean,
   },
-  setup(_,{emit}){
-    const store= useStore()
-    let cart:Product[]= store.state.cart
+  setup(_, { emit }) {
+    const store = useStore();
+    let cart: Product[] = store.state.cart;
     function closeDrawer() {
-           emit('close');
-  }
-  return {
-    cart,
-    closeDrawer
-  }
-}
+      emit("close");
+    }
+    return {
+      cart,
+      closeDrawer,
+    };
+  },
 });
 </script>
 
 <style scoped>
-h2{
-    margin-bottom: 0;
+h2 {
+  margin-bottom: 0;
 }
 .overlay {
   position: fixed;
@@ -67,19 +64,18 @@ h2{
   height: 100%;
   background: linear-gradient(to bottom, #1d2c3b, #0a1a24);
   color: white;
-  overflow:scroll;
+  overflow: scroll;
   z-index: 10;
-  padding:2px;
+  padding: 2px;
   text-align: center;
   transition: right 0.3s ease-in-out;
 }
-.drawer__line{
-    height:1px;
-    width:100%;
-    background-color: #fff;
+.drawer__line {
+  height: 1px;
+  width: 100%;
+  background-color: #fff;
 }
 .drawer.open {
   right: 0;
 }
-
 </style>
--- saqaya-ecommerce/src/views/productsPage.vue
+++ saqaya-ecommerce/src/views/productsPage.vue
@@ -4,92 +4,98 @@
     <sortDropdown @sortProducts="orderProducts"></sortDropdown>
   </div>
   <div class="products">
-   <div class="products__product" v-for="product in products" :key="product.id">
-    <productCard
-      :product="product" :clickable="true"
-    />
+    <div
+      class="products__product"
+      v-for="product in products"
+      :key="product.id"
+    >
+      <productCard :product="product" :clickable="true" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import productCard from '../components/product/productCard.vue';
-import productsHeader from '../components/product/productsHeader.vue';
-import {defineComponent} from 'vue';
-import {Product} from '../../public/interfaces/Product';
-import sortDropdown from '../components/product/sortDropdown.vue';
+import productCard from "../components/product/productCard.vue";
+import productsHeader from "../components/product/productsHeader.vue";
+import { defineComponent, ref } from "vue";
+import { Product } from "../../public/interfaces/Product";
+import sortDropdown from "../components/product/sortDropdown.vue";
+import { useStore } from "vuex";
 
 export default defineComponent({
-  name: 'productsPage',
+  name: "productsPage",
   components: {
     productCard,
     productsHeader,
-    sortDropdown
+    sortDropdown,
   },
-  data(){
-    return ({
-      selectedSort:""
-    })
-  },
-  props:{
-    clickable:{
-      type:Boolean,
-      required:false
+  setup() {
+    const store = useStore();
+    let selectedSort = ref("");
+    function orderProducts(value: string) {
+      selectedSort.value = value;
     }
-  },
-  methods:{
-         orderProducts(value:string){
-               this.selectedSort=value
-         }
-  },
-  computed:{
-      products():Product[] {
-        let products:Product[];
-        switch(this.selectedSort){
-          case "rate-ascending": products= this.$store.getters.sortProductsRatingAsc;
-                                 break;
-          case "rate-descending": products= this.$store.getters.sortProductsRatingDesc;
-                                  break;        
-          case "price-descending": products= this.$store.getters.sortProductsPriceDesc;
-                                  break;     
-          case "price-ascending": products= this.$store.getters.sortProductsPriceAsc;
-                                  break;            
-          default: products= this.$store.state.products                                                                                     
-        }
-        return products;
-      }
-  
-  },
 
-})
+    function fetchProductsBasedOnSort(): Product[] {
+      let products: Product[];
+      switch (selectedSort.value) {
+        case "rate-ascending":
+          products = store.getters.sortProductsRatingAsc;
+          break;
+        case "rate-descending":
+          products = store.getters.sortProductsRatingDesc;
+          break;
+        case "price-descending":
+          products = store.getters.sortProductsPriceDesc;
+          break;
+        case "price-ascending":
+          products = store.getters.sortProductsPriceAsc;
+          break;
+        default:
+          products = store.state.products;
+      }
+      return products;
+    }
+
+    let products: Product[] = fetchProductsBasedOnSort();
+    return {
+      selectedSort,
+      orderProducts,
+      products,
+    };
+  },
+  props: {
+    clickable: {
+      type: Boolean,
+      required: false,
+    },
+  },
+});
 </script>
 
 <style scoped lang="scss">
-
-.sort{
+.sort {
   display: flex;
   justify-content: flex-end;
-  width:90%;
-  gap:10px;
-  padding:20px;
+  width: 90%;
+  gap: 10px;
+  padding: 20px;
   align-items: center;
 }
-.products{
-  display:flex;
+.products {
+  display: flex;
   flex-direction: row;
-  gap:50px;
-  padding:20px;
+  gap: 50px;
+  padding: 20px;
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
 
-  &__product{
-    display:flex;
-    justify-content:center;
-    width:20%;
-    margin-bottom:20px;
-
-  
+  &__product {
+    display: flex;
+    justify-content: center;
+    width: 20%;
+    margin-bottom: 20px;
   }
 }
 
@@ -103,8 +109,5 @@ export default defineComponent({
   .products__product {
     width: 50%;
   }
-  
-  
 }
-
 </style>
