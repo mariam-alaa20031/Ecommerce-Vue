843e10f87da947be74237786b438fdec651aeb8a
mariam-alaa20031 <109764181+mariam-alaa20031@users.noreply.github.com>
2025-07-03 17:38:28 +0300

    fix import of store in router and migrate store in cartDrawer

--- saqaya-ecommerce/src/main.ts
+++ saqaya-ecommerce/src/main.ts
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import {createPinia} from 'pinia'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import '../public/styles/fonts.scss'
 import '../public/styles/global.scss'
 
 const pinia= createPinia()
-createApp(App).use(pinia).use(router).mount('#app')
+const app = createApp(App)
+app.use(pinia).use(router).mount('#app')
