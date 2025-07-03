3cae2db9c7c3ea6f389d58bcfa5e2e670ac9fb49
mariam-alaa20031 <109764181+mariam-alaa20031@users.noreply.github.com>
2025-07-03 18:02:27 +0300

    fix import axios error

--- saqaya-ecommerce/jest.config.ts
+++ saqaya-ecommerce/jest.config.ts
@@ -7,13 +7,13 @@ const config: Config.InitialOptions = {
     '^.+\\.vue$': '@vue/vue3-jest',
     '^.+\\.[tj]s?$': 'ts-jest',
   },
-  transformIgnorePatterns: [
-    '/node_modules/(?!(axios)/)',
-  ],
+  transformIgnorePatterns: ['node_modules/(?!(axios|axios/.+))'],
+
   moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
     '\\.(png|jpe?g|gif|webp|svg)$': '<rootDir>/tests/__mocks__/fileMock.ts',
+     "^axios$": "axios/dist/node/axios.cjs"
   },
 };
 
