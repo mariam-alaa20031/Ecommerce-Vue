886b25c89be576cba3e5b7f363620194a387a42e
mariam-alaa20031 <109764181+mariam-alaa20031@users.noreply.github.com>
2025-07-08 18:05:15 +0300

    adjust file with options to composition

--- saqaya-ecommerce/src/views/homePage.vue
+++ saqaya-ecommerce/src/views/homePage.vue
@@ -3,23 +3,9 @@
 <homeReviews></homeReviews>
 </template>
 
-
-<script lang="ts">
-
-import { defineComponent} from 'vue';
+<script lang="ts" setup>
 import homeHeader from  '../components/home/homeHeader.vue';
 import homeReviews from '../components/home/homeReviews.vue';
-
-export default defineComponent({
-  name: 'homePage',
-  components:{
-    homeHeader,
-    homeReviews
-  }
-})  
-
-
-
 </script>
 
 <style scoped lang="scss">
